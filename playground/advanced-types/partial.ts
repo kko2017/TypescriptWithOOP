fc75b4e47df4c97056ded7c2b88a0c596a15c7be
Alex KwangEun Oh <kwangeunoh1@gmail.com>
2021-02-16 20:44:32 -0400

    🔧 fix partial

--- playground/advanced-types/partial.ts
+++ playground/advanced-types/partial.ts
@@ -7,7 +7,12 @@
     };
 
     function updateTodo(todo: ToDo, fieldsToUpdate: Partial<ToDo>): ToDo {
-        todo = { ...todo, ...fieldsToUpdate };
+        // todo = { ...todo, ...fieldsToUpdate };
+
+        for (const key in fieldsToUpdate) {
+            todo[key] = fieldsToUpdate[key];
+        }
+        
         console.log(todo);
         return todo;
     }
@@ -18,10 +23,30 @@
         label: 'ts',
         prority: 'low'
     };
-
+    console.log(todo);
     const updated = updateTodo(todo, { prority: 'high' });
     console.log('----------------');
     console.log(updated);
     console.log('----------------');
     console.log(todo);
+
+    // function test(obj: any): void {
+    //     obj['a'] = 5;
+    // }
+
+    // let obj = {
+    //     a: 10
+    // };
+    // console.log(obj.a);
+    // test(obj);
+    // console.log(obj.a);
+
+    // let obj3 = {
+    //     a: 20
+    // };
+    // obj = {
+    //     ...obj,
+    //     ...obj3
+    // };
+    // console.log(obj.a);
 }
